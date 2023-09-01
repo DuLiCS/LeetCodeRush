e2eb657ca4d672a2912f016b075a0ae7fb74ef21
Li Du <ieee811@gmail.com>
2023-09-01 15:06:26 +0800

    88

---
+++ 88.py
@@ -0,0 +1,50 @@
+from typing import List
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        p1 = len(nums1) - 1
+        p2 = len(nums2) - 1
+
+        while p1 >= 0 and p2 >= 0:
+            if nums1[p1] == 0:
+                nums1[p1] = nums2[p2]
+                p1 -= 1
+                p2 -= 1
+            else:
+                p1 -=1
+
+        nums1.sort()
+        return nums1
+
+    def merge_1(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        p1 = m - 1
+        p2 = n - 1
+        p3 = m + n - 1
+
+        while p1 >= 0 and p2 >= 0:
+            if nums1[p1] > nums2[p2]:
+                nums1[p3] = nums1[p1]
+                p1 -= 1
+            else:
+                nums1[p3] = nums2[p2]
+                p2 -= 1
+            p3 -= 1
+
+        while p2 >= 0:
+            nums1[p] = nums2[p2]
+            p2 -= 1
+            p3 -= 1
+
+        return nums1
+
+nums1 = [1, 2, 3, 0, 0, 0]
+nums2 = [2, 5, 6]
+m = 3
+n = 3
+sol = Solution()
+print(sol.merge_1(nums1, m, nums2, n))
+
+
